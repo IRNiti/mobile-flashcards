36abf9a334c7e33f645d1cc6903289e7e3dd684d
IRNiti <ioana.niti@mail.mcgill.ca>
2020-06-15 23:05:21 -0700

    feat: adding animation to deck list

--- components/DeckCard.js
+++ components/DeckCard.js
@@ -5,8 +5,12 @@ import { defaultStyles } from './ContainerStyling'
 export default function DeckCard(props) {
     return(
         <View>
-            <Text style={defaultStyles.headerTxt}>{props.title}</Text>
-            <Text style={defaultStyles.descriptionTxt}>{props.numCards} {props.numCards === 1 ? 'card' : 'cards'}</Text>
+            <Text style={defaultStyles.headerTxt}>
+                {props.title}
+            </Text>
+            <Text style={defaultStyles.descriptionTxt}>
+                {props.numCards} {props.numCards === 1 ? 'card' : 'cards'}
+            </Text>
         </View>
     )
 }
--- components/DeckList.js
+++ components/DeckList.js
@@ -6,47 +6,56 @@ import { defaultStyles } from './ContainerStyling'
 import DeckCard from './DeckCard'
 
 class DeckList extends Component {
-    /*state = {
+    state = {
         opacity: new Animated.Value(1),
-        width: new Animated.Value(300),
-        height: new Animated.Value(300)
-    }*/
+    }
 
     componentDidMount() {
         this.props.dispatch(handleInitialData())
     }
 
     displayDeck = (key) => {
-        /*const { opacity, width, height } = this.state
-        Animated.timing(opacity, {toValue: 0, duration: 1000}).start()
-        Animated.spring(width, {toValue: 0, speed: 5}).start()
-        Animated.spring(height, {toValue: 0, speed: 5}).start()*/
+        const duration = 1000
+        const { opacity } = this.state
+        Animated.timing(opacity, {toValue: 0.01, duration: duration, useNativeDriver: true}).start()
 
-        this.props.navigation.navigate('DeckView', {
-            title: this.props.decks[key].title
-        })
+        setTimeout(() => {
+
+            this.props.navigation.navigate('DeckView', {
+                title: this.props.decks[key].title
+            })
+            this.state.opacity.setValue(1)
+        }, duration)
     }
 
     render() {
         const { decks } = this.props
+        const {opacity} = this.state
         return(
             <View>
                 {decks && Object.keys(decks).map((key) => (
                     <TouchableOpacity key={key}
                                       onPress={() => this.displayDeck(key)}
                     >
-                        <View style={defaultStyles.card}>
+                        <Animated.View style={[defaultStyles.card, {opacity}]}>
                             <DeckCard title={decks[key].title} numCards={decks[key].questions.length}/>
-                        </View>
+                        </Animated.View>
                     </TouchableOpacity>
                 ))}
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate('NewDeck')}
+                    >
+                        <Animated.View
+                            style={[
+                                defaultStyles.genericBtn,
+                                defaultStyles.submitBtn,
+                                styles.btnAlignment,
+                                {opacity}
+                                ]}>
+                            <Text style={defaultStyles.submitBtnText}>New Deck</Text>
+                        </Animated.View>
+                    </TouchableOpacity>
 
-                <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate('NewDeck')}
-                    style={[defaultStyles.genericBtn, defaultStyles.submitBtn, styles.btnAlignment]}
-                >
-                    <Text style={defaultStyles.submitBtnText}>New Deck</Text>
-                </TouchableOpacity>
             </View>
         )
     }
